4a53996f364076cfadb7a74c5cf228778b3a33fa
William Tange <will@brt.sexy>
2022-06-05 13:47:23 +0200

    Guess if input is text or binary
    Or rather check if all chars are ws or printable

--- src/bin/disasm.rs
+++ src/bin/disasm.rs
@@ -6,12 +6,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let stdin = std::io::stdin();
     let mut buf = vec![];
     stdin.lock().read_to_end(&mut buf)?;
-    for c in dasha::disasm(
-        buf.bytes()
-            .enumerate()
-            .map(|(idx, byte)| Spanning(byte.unwrap(), idx, 1, None)),
-    )? {
-        println!("{:#x}:\t{}", c.1, c);
+    if buf
+        .iter()
+        .all(|&b| (b as char).is_whitespace() || 0x20 <= b && b <= 0x7e)
+    {
+        // text
+        let text = std::str::from_utf8(&buf).unwrap(); // what about unicode?
+
+        for c in dasha::disasm(dasha::text::tokenize(text).unwrap())? {
+            println!("{:#x}:\t{}", c.1, c);
+        }
+    } else {
+        // binary
+        for c in dasha::disasm(
+            buf.bytes()
+                .enumerate()
+                .map(|(idx, byte)| Spanning(byte.unwrap(), idx, 1, None)),
+        )? {
+            println!("{:#x}:\t{}", c.1, c);
+        }
     }
     Ok(())
 }

c5d654301a0889375a72eb3d5841d941d526a37e
William Tange <will@brt.sexy>
2022-12-24 16:29:59 +0100

    Make Loc indexing zero-based
    This isn't Lua lol

--- src/text.rs
+++ src/text.rs
@@ -26,7 +26,7 @@ impl Loc {
     }
 
     fn shift_line(self, c: usize) -> Self {
-        Loc(self.0 + c, self.1 + 1, 1)
+        Loc(self.0 + c, self.1 + 1, 0)
     }
 }
 
@@ -38,12 +38,12 @@ impl fmt::Display for Loc {
 
 #[test]
 fn test_loc_shift() {
-    assert_eq!(Loc(0, 1, 1).shift(1), Loc(1, 1, 2));
+    assert_eq!(Loc(0, 0, 0).shift(1), Loc(1, 0, 1));
 }
 
 #[test]
 fn test_loc_shift_line() {
-    assert_eq!(Loc(0, 1, 1).shift_line(1), Loc(1, 2, 1));
+    assert_eq!(Loc(0, 0, 0).shift_line(1), Loc(1, 1, 0));
 }
 
 #[derive(Clone, Copy, PartialEq)]
@@ -162,162 +162,162 @@ impl<'a> Tokenizer<'a> {
 #[test]
 fn test_tokenizer_trim_start() {
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "",
     };
     t.trim_start();
-    assert_eq!(t.offset, Loc(0, 1, 1));
+    assert_eq!(t.offset, Loc(0, 0, 0));
     assert_eq!(t.text, "");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "  ",
     };
     t.trim_start();
-    assert_eq!(t.offset, Loc(2, 1, 3));
+    assert_eq!(t.offset, Loc(2, 0, 2));
     assert_eq!(t.text, "");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "\t\r\n",
     };
     t.trim_start();
-    assert_eq!(t.offset, Loc(2, 2, 1));
+    assert_eq!(t.offset, Loc(2, 1, 0));
     assert_eq!(t.text, "");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: " ;  ",
     };
     t.trim_start();
-    assert_eq!(t.offset, Loc(4, 1, 5));
+    assert_eq!(t.offset, Loc(4, 0, 4));
     assert_eq!(t.text, "");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: " ; hello world ",
     };
     t.trim_start();
-    assert_eq!(t.offset, Loc(15, 1, 16));
+    assert_eq!(t.offset, Loc(15, 0, 15));
     assert_eq!(t.text, "");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "1312 ; baca ",
     };
     t.trim_start();
-    assert_eq!(t.offset, Loc(0, 1, 1));
+    assert_eq!(t.offset, Loc(0, 0, 0));
     assert_eq!(t.text, "1312 ; baca ");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "\t\t  4200\n\t\t  1337",
     };
     t.trim_start();
-    assert_eq!(t.offset, Loc(4, 1, 5));
+    assert_eq!(t.offset, Loc(4, 0, 4));
     assert_eq!(t.text, "4200\n\t\t  1337");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "    f4c3 ; book\n    c00b ; ecaf\n",
     };
     t.trim_start();
-    assert_eq!(t.offset, Loc(4, 1, 5));
+    assert_eq!(t.offset, Loc(4, 0, 4));
     assert_eq!(t.text, "f4c3 ; book\n    c00b ; ecaf\n");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "; lorem ipsum\n; dolor sit amet\nffff",
     };
     t.trim_start();
-    assert_eq!(t.offset, Loc(31, 3, 1));
+    assert_eq!(t.offset, Loc(31, 2, 0));
     assert_eq!(t.text, "ffff");
 }
 
 #[test]
 fn test_tokenizer_take_byte() {
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "",
     };
     assert_eq!(t.take_byte(), None);
-    assert_eq!(t.offset, Loc(0, 1, 1));
+    assert_eq!(t.offset, Loc(0, 0, 0));
     assert_eq!(t.text, "");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "  ",
     };
     assert_eq!(t.take_byte(), None);
-    assert_eq!(t.offset, Loc(2, 1, 3));
+    assert_eq!(t.offset, Loc(2, 0, 2));
     assert_eq!(t.text, "");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "\t\r\n",
     };
     assert_eq!(t.take_byte(), None);
-    assert_eq!(t.offset, Loc(2, 2, 1));
+    assert_eq!(t.offset, Loc(2, 1, 0));
     assert_eq!(t.text, "");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: " ;  ",
     };
     assert_eq!(t.take_byte(), None);
-    assert_eq!(t.offset, Loc(4, 1, 5));
+    assert_eq!(t.offset, Loc(4, 0, 4));
     assert_eq!(t.text, "");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: " ; hello world ",
     };
     assert_eq!(t.take_byte(), None);
-    assert_eq!(t.offset, Loc(15, 1, 16));
+    assert_eq!(t.offset, Loc(15, 0, 15));
     assert_eq!(t.text, "");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "1312 ; baca ",
     };
     assert_eq!(
         t.take_byte(),
-        Some(Ok(Spanning(0x13, Loc(0, 1, 1), Loc(2, 1, 3), None))),
+        Some(Ok(Spanning(0x13, Loc(0, 0, 0), Loc(2, 0, 2), None))),
     );
-    assert_eq!(t.offset, Loc(2, 1, 3));
+    assert_eq!(t.offset, Loc(2, 0, 2));
     assert_eq!(t.text, "12 ; baca ");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "\t\t  4200\n\t\t  1337",
     };
     assert_eq!(
         t.take_byte(),
-        Some(Ok(Spanning(0x42, Loc(4, 1, 5), Loc(6, 1, 7), None))),
+        Some(Ok(Spanning(0x42, Loc(4, 0, 4), Loc(6, 0, 6), None))),
     );
-    assert_eq!(t.offset, Loc(6, 1, 7));
+    assert_eq!(t.offset, Loc(6, 0, 6));
     assert_eq!(t.text, "00\n\t\t  1337");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "    f4c3 ; book\n    c00b ; ecaf\n",
     };
     assert_eq!(
         t.take_byte(),
-        Some(Ok(Spanning(0xf4, Loc(4, 1, 5), Loc(6, 1, 7), None))),
+        Some(Ok(Spanning(0xf4, Loc(4, 0, 4), Loc(6, 0, 6), None))),
     );
-    assert_eq!(t.offset, Loc(6, 1, 7));
+    assert_eq!(t.offset, Loc(6, 0, 6));
     assert_eq!(t.text, "c3 ; book\n    c00b ; ecaf\n");
 
     let mut t = Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text: "; lorem ipsum\n; dolor sit amet\nffff",
     };
     assert_eq!(
         t.take_byte(),
-        Some(Ok(Spanning(0xff, Loc(31, 3, 1), Loc(33, 3, 3), None))),
+        Some(Ok(Spanning(0xff, Loc(31, 2, 0), Loc(33, 2, 2), None))),
     );
-    assert_eq!(t.offset, Loc(33, 3, 3));
+    assert_eq!(t.offset, Loc(33, 2, 2));
     assert_eq!(t.text, "ff");
 }
 
@@ -331,7 +331,7 @@ impl<'a> Iterator for Tokenizer<'a> {
 
 pub fn tokenize(text: &str) -> Result<Vec<Spanning<u8, Loc>>, Error> {
     Ok(Tokenizer {
-        offset: Loc(0, 1, 1),
+        offset: Loc(0, 0, 0),
         text,
     }
     .collect::<Result<Vec<_>, _>>()?)
@@ -341,36 +341,36 @@ pub fn tokenize(text: &str) -> Result<Vec<Spanning<u8, Loc>>, Error> {
 fn test_tokenize() {
     assert_eq!(
         tokenize("0").unwrap(),
-        vec![Spanning(0, Loc(0, 1, 1), Loc(1, 1, 2), None)],
+        vec![Spanning(0, Loc(0, 0, 0), Loc(1, 0, 1), None)],
     );
     assert_eq!(
         tokenize("00").unwrap(),
-        vec![Spanning(0, Loc(0, 1, 1), Loc(2, 1, 3), None)],
+        vec![Spanning(0, Loc(0, 0, 0), Loc(2, 0, 2), None)],
     );
     assert_eq!(
         tokenize("  00\r\n").unwrap(),
-        vec![Spanning(0, Loc(2, 1, 3), Loc(4, 1, 5), None)],
+        vec![Spanning(0, Loc(2, 0, 2), Loc(4, 0, 4), None)],
     );
     assert_eq!(
         tokenize("0 12 345 6789").unwrap(),
         vec![
-            Spanning(0, Loc(0, 1, 1), Loc(1, 1, 2), None),
-            Spanning(0x12, Loc(2, 1, 3), Loc(4, 1, 5), None),
-            Spanning(0x34, Loc(5, 1, 6), Loc(7, 1, 8), None),
-            Spanning(5, Loc(7, 1, 8), Loc(8, 1, 9), None),
-            Spanning(0x67, Loc(9, 1, 10), Loc(11, 1, 12), None),
-            Spanning(0x89, Loc(11, 1, 12), Loc(13, 1, 14), None),
+            Spanning(0, Loc(0, 0, 0), Loc(1, 0, 1), None),
+            Spanning(0x12, Loc(2, 0, 2), Loc(4, 0, 4), None),
+            Spanning(0x34, Loc(5, 0, 5), Loc(7, 0, 7), None),
+            Spanning(5, Loc(7, 0, 7), Loc(8, 0, 8), None),
+            Spanning(0x67, Loc(9, 0, 9), Loc(11, 0, 11), None),
+            Spanning(0x89, Loc(11, 0, 11), Loc(13, 0, 13), None),
         ],
     );
     assert_eq!(
         tokenize("1312 dead beef").unwrap(),
         vec![
-            Spanning(0x13, Loc(0, 1, 1), Loc(2, 1, 3), None),
-            Spanning(0x12, Loc(2, 1, 3), Loc(4, 1, 5), None),
-            Spanning(0xde, Loc(5, 1, 6), Loc(7, 1, 8), None),
-            Spanning(0xad, Loc(7, 1, 8), Loc(9, 1, 10), None),
-            Spanning(0xbe, Loc(10, 1, 11), Loc(12, 1, 13), None),
-            Spanning(0xef, Loc(12, 1, 13), Loc(14, 1, 15), None),
+            Spanning(0x13, Loc(0, 0, 0), Loc(2, 0, 2), None),
+            Spanning(0x12, Loc(2, 0, 2), Loc(4, 0, 4), None),
+            Spanning(0xde, Loc(5, 0, 5), Loc(7, 0, 7), None),
+            Spanning(0xad, Loc(7, 0, 7), Loc(9, 0, 9), None),
+            Spanning(0xbe, Loc(10, 0, 10), Loc(12, 0, 12), None),
+            Spanning(0xef, Loc(12, 0, 12), Loc(14, 0, 14), None),
         ],
     );
     assert_eq!(tokenize("; blank comment thing").unwrap(), vec![]);
@@ -398,37 +398,37 @@ fn test_tokenize() {
         )
         .unwrap(),
         vec![
-            Spanning(0x31, Loc(20, 2, 9), Loc(22, 2, 11), None),
-            Spanning(0xc0, Loc(23, 2, 12), Loc(25, 2, 14), None),
-            Spanning(0x50, Loc(71, 3, 9), Loc(73, 3, 11), None),
-            Spanning(0x50, Loc(117, 4, 9), Loc(119, 4, 11), None),
-            Spanning(0xb0, Loc(163, 5, 9), Loc(165, 5, 11), None),
-            Spanning(0x17, Loc(166, 5, 12), Loc(168, 5, 14), None),
-            Spanning(0xcd, Loc(214, 6, 9), Loc(216, 6, 11), None),
-            Spanning(0x80, Loc(217, 6, 12), Loc(219, 6, 14), None),
-            Spanning(0x31, Loc(301, 9, 9), Loc(303, 9, 11), None),
-            Spanning(0xc0, Loc(304, 9, 12), Loc(306, 9, 14), None),
-            Spanning(0x50, Loc(352, 10, 9), Loc(354, 10, 11), None),
-            Spanning(0x68, Loc(398, 11, 9), Loc(400, 11, 11), None),
-            Spanning(0x2f, Loc(401, 11, 12), Loc(403, 11, 14), None),
-            Spanning(0x2f, Loc(404, 11, 15), Loc(406, 11, 17), None),
-            Spanning(0x73, Loc(407, 11, 18), Loc(409, 11, 20), None),
-            Spanning(0x68, Loc(410, 11, 21), Loc(412, 11, 23), None),
-            Spanning(0x68, Loc(451, 12, 9), Loc(453, 12, 11), None),
-            Spanning(0x2f, Loc(454, 12, 12), Loc(456, 12, 14), None),
-            Spanning(0x62, Loc(457, 12, 15), Loc(459, 12, 17), None),
-            Spanning(0x69, Loc(460, 12, 18), Loc(462, 12, 20), None),
-            Spanning(0x6e, Loc(463, 12, 21), Loc(465, 12, 23), None),
-            Spanning(0x89, Loc(504, 13, 9), Loc(506, 13, 11), None),
-            Spanning(0xe3, Loc(507, 13, 12), Loc(509, 13, 14), None),
-            Spanning(0x50, Loc(555, 14, 9), Loc(557, 14, 11), None),
-            Spanning(0x54, Loc(601, 15, 9), Loc(603, 15, 11), None),
-            Spanning(0x53, Loc(647, 16, 9), Loc(649, 16, 11), None),
-            Spanning(0x50, Loc(693, 17, 9), Loc(695, 17, 11), None),
-            Spanning(0xb0, Loc(739, 18, 9), Loc(741, 18, 11), None),
-            Spanning(0x3b, Loc(742, 18, 12), Loc(744, 18, 14), None),
-            Spanning(0xcd, Loc(790, 19, 9), Loc(792, 19, 11), None),
-            Spanning(0x80, Loc(793, 19, 12), Loc(795, 19, 14), None),
+            Spanning(0x31, Loc(20, 1, 8), Loc(22, 1, 10), None),
+            Spanning(0xc0, Loc(23, 1, 11), Loc(25, 1, 13), None),
+            Spanning(0x50, Loc(71, 2, 8), Loc(73, 2, 10), None),
+            Spanning(0x50, Loc(117, 3, 8), Loc(119, 3, 10), None),
+            Spanning(0xb0, Loc(163, 4, 8), Loc(165, 4, 10), None),
+            Spanning(0x17, Loc(166, 4, 11), Loc(168, 4, 13), None),
+            Spanning(0xcd, Loc(214, 5, 8), Loc(216, 5, 10), None),
+            Spanning(0x80, Loc(217, 5, 11), Loc(219, 5, 13), None),
+            Spanning(0x31, Loc(301, 8, 8), Loc(303, 8, 10), None),
+            Spanning(0xc0, Loc(304, 8, 11), Loc(306, 8, 13), None),
+            Spanning(0x50, Loc(352, 9, 8), Loc(354, 9, 10), None),
+            Spanning(0x68, Loc(398, 10, 8), Loc(400, 10, 10), None),
+            Spanning(0x2f, Loc(401, 10, 11), Loc(403, 10, 13), None),
+            Spanning(0x2f, Loc(404, 10, 14), Loc(406, 10, 16), None),
+            Spanning(0x73, Loc(407, 10, 17), Loc(409, 10, 19), None),
+            Spanning(0x68, Loc(410, 10, 20), Loc(412, 10, 22), None),
+            Spanning(0x68, Loc(451, 11, 8), Loc(453, 11, 10), None),
+            Spanning(0x2f, Loc(454, 11, 11), Loc(456, 11, 13), None),
+            Spanning(0x62, Loc(457, 11, 14), Loc(459, 11, 16), None),
+            Spanning(0x69, Loc(460, 11, 17), Loc(462, 11, 19), None),
+            Spanning(0x6e, Loc(463, 11, 20), Loc(465, 11, 22), None),
+            Spanning(0x89, Loc(504, 12, 8), Loc(506, 12, 10), None),
+            Spanning(0xe3, Loc(507, 12, 11), Loc(509, 12, 13), None),
+            Spanning(0x50, Loc(555, 13, 8), Loc(557, 13, 10), None),
+            Spanning(0x54, Loc(601, 14, 8), Loc(603, 14, 10), None),
+            Spanning(0x53, Loc(647, 15, 8), Loc(649, 15, 10), None),
+            Spanning(0x50, Loc(693, 16, 8), Loc(695, 16, 10), None),
+            Spanning(0xb0, Loc(739, 17, 8), Loc(741, 17, 10), None),
+            Spanning(0x3b, Loc(742, 17, 11), Loc(744, 17, 13), None),
+            Spanning(0xcd, Loc(790, 18, 8), Loc(792, 18, 10), None),
+            Spanning(0x80, Loc(793, 18, 11), Loc(795, 18, 13), None),
         ],
     );
 }
